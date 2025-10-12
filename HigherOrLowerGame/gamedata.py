f4e3e6ef74664d1fe541b1a7cc628e09a35aac72
Tanmay Kapoor <tk5996@srmist.edu.in>
2025-10-13 02:16:01 +0530

    Higher Lower Game

---
+++ HigherOrLowerGame/gamedata.py
@@ -0,0 +1,122 @@
+data = [
+    {
+        'name': 'Virat Kohli',
+        'follower_count': 270,
+        'description': 'Cricketer',
+        'country': 'India'
+    },
+    {
+        'name': 'Narendra Modi',
+        'follower_count': 250,
+        'description': 'Prime Minister of India',
+        'country': 'India'
+    },
+    {
+        'name': 'Priyanka Chopra',
+        'follower_count': 90,
+        'description': 'Actress and singer',
+        'country': 'India'
+    },
+    {
+        'name': 'Alia Bhatt',
+        'follower_count': 85,
+        'description': 'Actress',
+        'country': 'India'
+    },
+    {
+        'name': 'Shraddha Kapoor',
+        'follower_count': 90,
+        'description': 'Actress and singer',
+        'country': 'India'
+    },
+    {
+        'name': 'Deepika Padukone',
+        'follower_count': 80,
+        'description': 'Actress and model',
+        'country': 'India'
+    },
+    {
+        'name': 'MS Dhoni',
+        'follower_count': 50,
+        'description': 'Former Indian cricket captain',
+        'country': 'India'
+    },
+    {
+        'name': 'Sachin Tendulkar',
+        'follower_count': 45,
+        'description': 'Cricket legend',
+        'country': 'India'
+    },
+    {
+        'name': 'Katrina Kaif',
+        'follower_count': 75,
+        'description': 'Actress',
+        'country': 'India'
+    },
+    {
+        'name': 'Akshay Kumar',
+        'follower_count': 70,
+        'description': 'Actor and producer',
+        'country': 'India'
+    },
+    {
+        'name': 'Neha Kakkar',
+        'follower_count': 75,
+        'description': 'Singer',
+        'country': 'India'
+    },
+    {
+        'name': 'CarryMinati',
+        'follower_count': 40,
+        'description': 'YouTuber and streamer',
+        'country': 'India'
+    },
+    {
+        'name': 'Amitabh Bachchan',
+        'follower_count': 60,
+        'description': 'Actor and TV host',
+        'country': 'India'
+    },
+    {
+        'name': 'Samantha Ruth Prabhu',
+        'follower_count': 35,
+        'description': 'Actress',
+        'country': 'India'
+    },
+    {
+        'name': 'T-Series',
+        'follower_count': 250,
+        'description': 'Music and film production company',
+        'country': 'India'
+    },
+    {
+        'name': 'Cricket India (BCCI)',
+        'follower_count': 35,
+        'description': 'Official cricket board of India',
+        'country': 'India'
+    },
+    {
+        'name': 'Anushka Sharma',
+        'follower_count': 65,
+        'description': 'Actress and producer',
+        'country': 'India'
+    },
+    {
+        'name': 'Shah Rukh Khan',
+        'follower_count': 75,
+        'description': 'Actor and film producer',
+        'country': 'India'
+    },
+    {
+        'name': 'KL Rahul',
+        'follower_count': 30,
+        'description': 'Cricketer',
+        'country': 'India'
+    },
+    {
+        'name': 'Rohit Sharma',
+        'follower_count': 35,
+        'description': 'Cricketer and Indian team captain',
+        'country': 'India'
+    }
+]
