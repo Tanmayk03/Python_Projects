bf509ea3eaa0b2da7698cc17109406567c54dfe9
Tanmay Kapoor <tk5996@srmist.edu.in>
2025-10-27 22:55:30 +0530

    Updated

--- BettingTurtle/app.py
+++ BettingTurtle/app.py
@@ -3,29 +3,39 @@ import random
 
 screen = Screen()
 screen.setup(width=500, height=400)
+
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []  # 🐢 Store all turtle objects
 
-for turtle_index in range(0, 6):
+# Create turtles
+for turtle_index in range(6):
     new_tim = Turtle(shape="turtle")
     new_tim.penup()
     new_tim.color(colors[turtle_index])
     new_tim.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_tim)  # Add each turtle to the list
+
+is_race_on = False
 
 if user_bet:
     is_race_on = True   
 
+# Start race
 while is_race_on:
     for turtle in all_turtles:
-       if turtle.xcor() > 230:
-           winning_color = turtle.pencolor()
-           if winning_color == user_bet:
-               print(f"You've won! The {winning_color} turtle is the winner!")
-           else:
-               print(f"You've lost! The {winning_color} turtle is the winner!")
-           is_race_on = False
+        if turtle.xcor() > 230:
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
+            is_race_on = False
+            break  # stop checking others
         
         random_distance = random.randint(0, 10)
         turtle.forward(random_distance)
+
 screen.exitonclick()
