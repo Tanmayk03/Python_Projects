bd8269d62e1e789b620ef4e641438ce77ecdee69
Tanmay Kapoor <142162139+Tanmayk03@users.noreply.github.com>
2025-11-05 19:18:15 +0530

    Updated

--- SnakeGame/app.py
+++ SnakeGame/app.py
@@ -1,10 +1,11 @@
 from turtle import Screen, Turtle
-from time import sleep
+import time
 
 screen = Screen()
 screen.title("Snake Game")
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
+screen.tracer(0)
 
 starting_position = [(0, 0 ), (-20, 0), (-40, 0)]
 segments = []
@@ -16,27 +17,15 @@ for position in starting_position:
     new_segment.goto(position)
     segments.append(new_segment)
 
+screen.update()
+
 game_is_on = True
 while game_is_on:
+    screen.update()
+    time.sleep(0.1)
     for seg in segments:
         seg.forward(20)
 
-break
 
-# start a proper game loop (we broke out of the previous placeholder loop above)
-screen.tracer(0)
-
-head = segments[0]
-
-while True:
-# move each segment to the position of the segment in front of it
-for idx in range(len(segments) - 1, 0, -1):
-    new_x = segments[idx - 1].xcor()
-    new_y = segments[idx - 1].ycor()
-    segments[idx].goto(new_x, new_y)
-head.forward(20)
-
-screen.update()
-sleep(0.1)
 
 screen.exitonclick()
