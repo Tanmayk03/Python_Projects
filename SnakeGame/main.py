403ec8c98aa5ec4da15ea141e6beb62e2a4b313b
Tanmay Kapoor <142162139+Tanmayk03@users.noreply.github.com>
2025-11-08 00:39:23 +0530

    Updated

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -10,11 +10,12 @@ screen.tracer(0)
 
 snake = Snake()
 
+# Activate key listener
 screen.listen()
-screen.onkeypress(snake.up, "Up")
-screen.onkeypress(snake.down, "Down")
-screen.onkeypress(snake.left, "Left")
-screen.onkeypress(snake.right, "Right")
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 
@@ -22,5 +23,11 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    # Detect wall collision
+    if (snake.head.xcor() > 280 or snake.head.xcor() < -280 or
+        snake.head.ycor() > 280 or snake.head.ycor() < -280):
+        game_is_on = False
+        screen.textinput("Game Over", "You hit the wall! Press Enter to exit.")
 
 screen.exitonclick()
