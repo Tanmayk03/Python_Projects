9d9560cee06332a37b28ba8326144035275fcddd
Tanmay Kapoor <142162139+Tanmayk03@users.noreply.github.com>
2025-11-07 22:43:47 +0530

    Updated

---
+++ SnakeGame/main.py
@@ -0,0 +1,26 @@
+from turtle import Screen
+import time
+from snake import Snake
+
+screen = Screen()
+screen.title("Snake Game")
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkeypress(snake.up, "Up")
+screen.onkeypress(snake.down, "Down")
+screen.onkeypress(snake.left, "Left")
+screen.onkeypress(snake.right, "Right")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+screen.exitonclick()
